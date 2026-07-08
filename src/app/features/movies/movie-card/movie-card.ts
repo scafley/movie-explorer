06c8d64f2a5f3025eb598e95945e9534b848d739
Dominik <d.nalepa1337@gmail.com>
2026-07-08 20:00:43 +0000

    feat: make movie card navigate to details

--- src/app/features/movies/movie-card/movie-card.ts
+++ src/app/features/movies/movie-card/movie-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from '../movie.models';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss',
 })
