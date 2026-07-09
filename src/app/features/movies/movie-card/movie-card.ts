94e74e43f7c94900fc789c8e212bf3ba60836a66
Dominik <d.nalepa1337@gmail.com>
2026-07-09 20:07:27 +0000

    feat: add fav toggle button to movie card

--- src/app/features/movies/movie-card/movie-card.ts
+++ src/app/features/movies/movie-card/movie-card.ts
@@ -1,14 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Movie } from '../movie.models';
-import { NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FavoritesStore } from '../favorites-store';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage, RouterLink, AsyncPipe],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss',
 })
 export class MovieCard {
   @Input({ required: true }) movie!: Movie;
+
+  protected readonly favorites = inject(FavoritesStore);
+
+  onToggleFavorite(event: Event): void {
+    event.stopPropagation();
+    event.preventDefault();
+    this.favorites.toggle(this.movie);
+  }
 }
